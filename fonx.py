1329a697133b58901af87fe5b9f34fcecb0cac57
elfsmyr <elfsmyrshn@gmail.com>
2022-06-12 23:22:18 +0200

    fonk2

--- fonx.py
+++ fonx.py
@@ -8,40 +8,94 @@ def connect():
             password="me5842")
 
         cur = conn.cursor()
+        
         table=input("tablo:")
+        #studentlogin
         if table=='students':
-            id=int(input("id:"))
+            student_number=int(input("studentnumber:"))
             password=input("password:")
             cur.execute("""
             select * from students
             where studentid={} and password_=MD5('{}')
-            """.format(id,password),
+            """.format(student_number,password),
             )
-            student = cur.fetchone()
+            student = cur.fetchall()
             if student==[]:
-                print("sifre yanlıs yada kullanıcı bulunamadı")
+                print("password or username is wrong")
             else:
                 print(student)
+        #teacherlogin
         elif table=='teachers':
             username=input("username:")
             password=input("password:")
             cur.execute("""
-            select * from teachers
+            select teacherid,username,teachername,teachersurname,dateofbirth from teachers
             where username='{}' and password_=MD5('{}')
             """.format(username,password),
             )
-            teacher = cur.fetchone()
+            teacher = cur.fetchall()
             if teacher==[]:
-                print("sifre yanlıs yada kullanıcı bulunamadı")
+                print("password or username is wrong")
             else:
-                print(teacher)        
-        
-
+                print("""username={}
+name={}
+surname={}
+date of birth={}""".format(teacher[0][1],teacher[0][2].upper(),teacher[0][3].upper(),teacher[0][4]))  
+                
+                
         cur.execute("""
-            select * from {}
-            """.format(table),
+            select * from lessons where teacherid='{}'""".format(teacher[0][0])
+            )
+          
+        lesson = cur.fetchall()
+        print(lesson)
+        
+        choose=input("ne istiyon:")
+        
+        if choose=="add":
+            lesson_name=input("hangi ders:")
+            cur.execute("""
+            insert into lessons(lessonname,teacherid,classid)
+            values('{}',{},2)
+            """.format(lesson_name,teacher[0][0])
+            )
             
-        )
+        elif choose=="delete":
+            lesson_name=input("hangi ders:")
+            cur.execute("""
+            select lessonid from lessons where lessonname='{}'""".format(lesson_name)
+            )
+            lesson_id=cur.fetchall()
+            
+            cur.execute("""
+            DELETE FROM lessons WHERE lessonid='{}' ;
+            """.format(lesson_id[0][0])
+            )
+        
+        elif choose=="renew":
+            lesson_name=input("hangi ders:")
+            cur.execute("""
+            select lessonid from lessons where lessonname='{}'""".format(lesson_name)
+            )
+            lesson_id=cur.fetchall()
+            new_name=input("name:")
+            new_class=input("class:")
+            
+            cur.execute(""" update lessons set lessonname='{}', classid={} WHERE lessonid={}
+                        """.format(new_name,new_class,lesson_id[0][0]))
+        
+        cur.execute("""
+            select * from lessons where teacherid='{}'""".format(teacher[0][0])
+            )
+        lesson = cur.fetchall()
+        print(lesson)
+            
+
+        # cur.execute("""
+        #     select * from {}
+        #     """.format(table),
+            
+        # )
         # students = cur.fetchall()
         # for all in students:
         #     print(all)   
