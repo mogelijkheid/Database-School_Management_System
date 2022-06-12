a2c0b22b5511a1f70cc9c7f5ebf03c0cbf36b60e
inci <altinisikyunusss@gmail.com>
2022-06-12 15:33:57 +0200

    login pages updated

--- student.py
+++ student.py
@@ -1,6 +1,13 @@
 from PyQt5 import QtWidgets,uic
-
+import entry
 class StudentWindow(QtWidgets.QMainWindow):
         def __init__(self):
             super(StudentWindow, self).__init__()
-            uic.loadUi('Ui/student.ui', self)
+            uic.loadUi('Ui/loginstudent.ui', self)
+            self.sbackbutton.clicked.connect(self.entry_show)
+        
+        def entry_show(self):
+            self.inci=entry.EntryWindow()
+            self.inci.show()
+            self.close()
+         
